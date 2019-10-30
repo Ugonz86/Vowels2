d2de17dfa12466f202c3c68ec75c73bebbe4f21d
Student <example@example.com>
2019-10-30 10:09:23 -0700

    Add looping function to vowel tracker.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
-
-
+  $("form#userForm").submit(function(event) {
+    event.preventDefault();
+    var userInput = $("#userInput").val();
+    var result = vowelCount (userInput);
+      $("#result").text(result);
+      console.log(result);
+  });
 
 });
+
+function vowelCount (textInput) {
+  var vowels = 0;
+  for (var i = 0; i < textInput.length; i++) {
+    if(textInput.charAt(i) == 'a' || textInput.charAt(i) == 'e' || textInput.charAt(i) == 'i' || textInput.charAt(i) == 'o' || textInput.charAt(i) == 'u' || textInput.charAt(i) == 'A' || textInput.charAt(i) == 'E' || textInput.charAt(i) == 'I' || textInput.charAt(i) == 'O' || textInput.charAt(i) == 'U') {
+         vowels++;
+   }
+  }
+  return vowels;
+}
